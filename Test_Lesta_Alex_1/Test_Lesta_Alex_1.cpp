dd6e186d02a6fb96cfd07d7fae14739f6e5fbd91
Александр Стародубов <alexstar.97@mail.ru>
2024-02-03 18:13:51 +0300

    Добавьте файлы проекта.

---
+++ Test_Lesta_Alex_1/Test_Lesta_Alex_1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+bool isEven(int value)
+{
+	div_t result = div(value, 2);
+	cout << "Integer: " << result.quot << endl;
+	cout << "Remains: " << result.rem << endl;
+	return !result.rem;
+}
+
+int main()
+{
+	int num;
+	cout << "Enter the number: ";
+	cin >> num;
+
+	if (isEven(num))
+	{
+		cout << num << " - YES";
+	}
+	else
+	{
+		cout << num << " - NO";
+	}
+}
